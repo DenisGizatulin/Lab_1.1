0c965f45a4b53678f117fffb469b4b6eda8d6d4a
DenisGizatulin <denis_gizatulin@outlook.com>
2024-11-07 00:21:38 +0300

    decorators and setters added

--- classes.py
+++ classes.py
@@ -1,4 +1,4 @@
-class Vehicle: # Создано базовый класс транспорта
+class Vehicle: # Создан базовый класс транспорта
     def __init__(self, name, capacity) -> None:
         self.name = name
         self.capacity = capacity
@@ -8,6 +8,26 @@ class Vehicle: # Создано базовый класс транспорта
 
     def info(self) -> None:
         print(f"Название: {self._name}, Вместимость: {self.capacity} единиц")
+
+    @property # Добавлен декоратор для того, чтобы name стал свойством и была возможность установки значения этого поля сеттером
+    def name(self) -> str:
+        return self._name
+
+    @name.setter # Сеттер для установки имени
+    def name(self, name) -> None:
+        if not isinstance(name, str):
+            raise ValueError("Название должно быть строкой!")
+        self._name = name
+
+    @property # Аналогичный декоратор
+    def capacity(self) -> int:
+        return self._capacity
+
+    @capacity.setter # Аналогичный сеттер
+    def age(self, capacity) -> None:
+        if not isinstance(capacity, int):
+            raise ValueError("")
+        self._capacity = capacity
 
 
 class Car(Vehicle): # Далее идут наследованные от Vehicle классы
