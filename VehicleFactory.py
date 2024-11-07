5761d777d86a4f05000c2c356f8a06916465f1be
DenisGizatulin <denis_gizatulin@outlook.com>
2024-11-07 17:23:24 +0300

    Added file type verification

--- VehicleFactory.py
+++ VehicleFactory.py
@@ -1,5 +1,5 @@
 from classes import Vehicle, Car, Truck, Bus, Bicycle, Motorcycle, MetroTrain, Train, Ship, Plane
-from exceptions import InvalidVehicleTypeException
+from exceptions import InvalidVehicleTypeException, InvalidFileTypeException
 import json
 import xml.etree.ElementTree as ET
 
@@ -26,41 +26,53 @@ class VehicleFactory:
 
     @staticmethod
     def save_to_json(vehicles, filename): # Метод для сохренения созданных объектов в json
-        with open(filename, 'w') as file:
-            json.dump([v.to_dict() for v in vehicles], file)
+        if '.json' not in filename:
+            raise InvalidFileTypeException(filename)
+        else:
+            with open(filename, 'w') as file:
+                json.dump([v.to_dict() for v in vehicles], file)
 
     @staticmethod
     def load_from_json(filename): # Метод для выгрузки объектов из json
-        with open(filename, 'r') as file:
-            vehicles_data = json.load(file)
-            return [VehicleFactory.create_vehicle(v['type'], v['name'], v['capacity']) for v in vehicles_data]
+        if '.json' not in filename:
+            raise InvalidFileTypeException(filename)
+        else:
+            with open(filename, 'r') as file:
+                vehicles_data = json.load(file)
+                return [VehicleFactory.create_vehicle(v['type'], v['name'], v['capacity']) for v in vehicles_data]
 
     @staticmethod
     def save_to_xml(vehicles, filename): # Метод для сохренения созданных объектов в xml
-        root = ET.Element('Vehicles')
-        for vehicle in vehicles:
-            vehicle_elem = ET.SubElement(root, 'Vehicle')
-            type_elem = ET.SubElement(vehicle_elem, 'Type')
-            name_elem = ET.SubElement(vehicle_elem, 'Name')
-            capacity_elem = ET.SubElement(vehicle_elem, 'Capacity')
+        if '.xml' not in filename:
+            raise InvalidFileTypeException(filename)
+        else:
+            root = ET.Element('Vehicles')
+            for vehicle in vehicles:
+                vehicle_elem = ET.SubElement(root, 'Vehicle')
+                type_elem = ET.SubElement(vehicle_elem, 'Type')
+                name_elem = ET.SubElement(vehicle_elem, 'Name')
+                capacity_elem = ET.SubElement(vehicle_elem, 'Capacity')
 
-            type_elem.text = vehicle.__class__.__name__
-            name_elem.text = vehicle.name
-            capacity_elem.text = str(vehicle.capacity)
+                type_elem.text = vehicle.__class__.__name__
+                name_elem.text = vehicle.name
+                capacity_elem.text = str(vehicle.capacity)
 
-        tree = ET.ElementTree(root)
-        tree.write(filename)
+            tree = ET.ElementTree(root)
+            tree.write(filename)
 
     @staticmethod
     def load_from_xml(filename): # Метод для выгрузки объектов из xml
-        tree = ET.parse(filename)
-        root = tree.getroot()
-        vehicles = []
+        if '.xml' not in filename:
+            raise InvalidFileTypeException(filename)
+        else:
+            tree = ET.parse(filename)
+            root = tree.getroot()
+            vehicles = []
 
-        for vehicle_elem in root.findall('Vehicle'):
-            vehicle_type = vehicle_elem.find('Type').text
-            name = vehicle_elem.find('Name').text
-            capacity = int(vehicle_elem.find('Capacity').text)
-            vehicles.append(VehicleFactory.create_vehicle(vehicle_type, name, capacity))
+            for vehicle_elem in root.findall('Vehicle'):
+                vehicle_type = vehicle_elem.find('Type').text
+                name = vehicle_elem.find('Name').text
+                capacity = int(vehicle_elem.find('Capacity').text)
+                vehicles.append(VehicleFactory.create_vehicle(vehicle_type, name, capacity))
 
-        return vehicles
+            return vehicles
